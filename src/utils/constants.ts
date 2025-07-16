e8730f93c5fd7e7e88de9476e3a93cd2e9b597ab
britoskies <britoskies@gmail.com>
2025-07-16 12:13:22 -0400

    feature #0009 - fix api url sintax error

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,4 +4,4 @@ export const AVATAR_URL = "https://i.pravatar.cc/40";
 const host = window.location.hostname;
 export const API_URL = host === 'localhost' || host === '127.0.0.1'
   ? 'http://localhost:3000/requests'
-  : 'https://parvaltest.netlify.app/netlify/functions/api';
+  : 'https://parvaltest.netlify.app/.netlify/functions/api';

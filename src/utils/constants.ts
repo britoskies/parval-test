ef496b5e0ebcb750945c7144b8c61e5f1e333342
britoskies <britoskies@gmail.com>
2025-07-16 15:07:42 -0400

    feature #0010 - added new mock data service for prod

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,7 +1,7 @@
 export const ITEMS_PER_PAGE = 10;
 export const AVATAR_URL = "https://i.pravatar.cc/40";
 
-const host = window.location.hostname;
-export const API_URL = host === 'localhost' || host === '127.0.0.1'
-  ? 'http://localhost:3000/requests'
-  : 'https://parvaltest.netlify.app/.netlify/functions/api';
+export const API_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000/requests"
+    : "https://6877eca1dba809d901f16af2.mockapi.io/requests";

96a68626fc78967e872d19fef4eefefb03cf46be
britoskies <britoskies@gmail.com>
2025-07-16 11:54:34 -0400

    feature #0005 - adjustment to handle api url in prod

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,7 @@
 export const ITEMS_PER_PAGE = 10;
 export const AVATAR_URL = "https://i.pravatar.cc/40";
-export const API_URL = "http://localhost:3000/requests";
+
+const host = window.location.hostname;
+export const API_URL = host === 'localhost' || host === '127.0.0.1'
+  ? 'http://localhost:3000/requests'
+  : 'https://parvaltest.netlify.app/.netlify/functions/api';
